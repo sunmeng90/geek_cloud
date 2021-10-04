c3850305be81b643b030552d31975871ce2fc830
Meng <sunmeng90@126.com>
2021-10-04 14:46:05 +0800

    week02 httpserver: 1. write env version back to client 2. add health check

--- week2/httpserver.go
+++ week2/httpserver.go
@@ -4,21 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	http.HandleFunc("/echo", func(rw http.ResponseWriter, r *http.Request) {
-		log.Printf("Request from: %s", r.Header.Get("Request-From"))
-		rw.Header().Set("Response-From", "go-server")
-		rw.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-		rw.WriteHeader(200)
-		rw.Header().Set("Response-Trailer1", "val1") // will not sent to client
-		fmt.Fprintf(rw, "Greeting: %s. Time: %s", r.URL.Query().Get("name"), time.Now().Format("2006-01-02 15:04:05 -0700 MST"))
-		rw.Header().Set("Response-Trailer2", "val2") // will not sent to client
-
-	})
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/healthz", healthz)
 	if err := http.ListenAndServe(":8090", nil); err != nil {
 		log.Fatal("Start up failed", err)
 	}
+}
+
+func healthz(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+}
+
+func echo(rw http.ResponseWriter, r *http.Request) {
+	log.Printf("Request from: %s", r.Header.Get("Request-From"))
+	rw.Header().Set("Response-From", "go-server")
+	env, _ := os.LookupEnv("VERSION")
+	rw.Header().Set("Version", env)
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	rw.WriteHeader(200)
+	rw.Header().Set("Response-Trailer1", "val1") // will not sent to client
+	fmt.Fprintf(rw, "Greeting: %s. Time: %s", r.URL.Query().Get("name"), time.Now().Format("2006-01-02 15:04:05 -0700 MST"))
+	rw.Header().Set("Response-Trailer2", "val2") // will not sent to client
 }
